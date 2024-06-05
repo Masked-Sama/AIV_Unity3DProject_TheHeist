92ac40aa918b7550cc9c5abfb7634b008a712970
FabioGiannino <fabiogiannino2@gmail.com>
2024-06-05 10:57:35 +0200

    add rotation to items and test change weapon

--- The Heist/Assets/Scripts/InventorySystem/Item.cs
+++ The Heist/Assets/Scripts/InventorySystem/Item.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    private const float speedRotation = 100;
+
     [SerializeField]
     private ItemObject itemObj;
     [Range(1,30)]
@@ -9,5 +11,10 @@ public class Item : MonoBehaviour
     private int quantity;
 
     public ItemObject ItemObj {  get { return itemObj; } }
-    public int Quantity { get { return quantity; } }    
+    public int Quantity { get { return quantity; } }
+
+    private void Update()
+    {
+        transform.Rotate(0,Time.deltaTime * speedRotation,0);
+    }
 }
