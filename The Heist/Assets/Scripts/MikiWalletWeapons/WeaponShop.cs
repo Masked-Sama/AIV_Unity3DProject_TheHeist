23833d2d606ec272b1e31c9f92e6e4b7a1497507
MikiVernox <mikivernox@gmail.com>
2024-06-11 16:01:10 +0200

    Market + scene

--- The Heist/Assets/Scripts/MikiWalletWeapons/WeaponShop.cs
+++ The Heist/Assets/Scripts/MikiWalletWeapons/WeaponShop.cs
@@ -9,9 +9,13 @@ public class WeaponShop : MonoBehaviour
     public GameObject infoText;
     [SerializeField]
     public PlayerCurrency playerCurrency;
+    public Transform weaponHoldPoint; // Punto di attacco per l'arma sul giocatore
 
     private PlayerInputActions inputActions;
     private bool isInteracting;
+    private Weapon currentWeapon;
+    private Weapon equippedWeapon; // L'arma attualmente equipaggiata
+    private Weapon lastWeaponSold;
 
     private void Awake()
     {
@@ -23,6 +27,7 @@ public class WeaponShop : MonoBehaviour
         inputActions.Enable();
         inputActions.Player.Interact.performed += OnInteractPerformed;
         inputActions.Player.Interact.canceled += OnInteractCanceled;
+
     }
 
     private void OnDisable()
@@ -30,7 +35,10 @@ public class WeaponShop : MonoBehaviour
         inputActions.Player.Interact.performed -= OnInteractPerformed;
         inputActions.Player.Interact.canceled -= OnInteractCanceled;
         inputActions.Disable();
+
     }
+
+
 
     private void OnInteractPerformed(InputAction.CallbackContext context)
     {
@@ -48,11 +56,13 @@ public class WeaponShop : MonoBehaviour
         Ray ray = new Ray(playerCamera.position, playerCamera.forward);
         RaycastHit hit;
 
+
         if (Physics.Raycast(ray, out hit, interactionDistance, weaponLayer))
         {
             Weapon weapon = hit.collider.GetComponent<Weapon>();
-            if (weapon != null)
+            if (weapon != null && weapon != equippedWeapon) // Impedisce di acquistare l'arma equipaggiata
             {
+                currentWeapon = weapon;
                 infoText.SetActive(true);
                 infoText.GetComponent<UnityEngine.UI.Text>().text = $"{weapon.weaponName} - Cost: {weapon.cost}";
 
@@ -71,8 +81,7 @@ public class WeaponShop : MonoBehaviour
 
     private void PurchaseWeapon(Weapon weapon)
     {
-         //playerCurrency = GetComponent<PlayerCurrency>();
-         PlayerCurrency soldi = playerCurrency.GetComponent<PlayerCurrency>();
+        PlayerCurrency soldi = playerCurrency.GetComponent<PlayerCurrency>();
 
         if (soldi != null)
         {
@@ -81,36 +90,51 @@ public class WeaponShop : MonoBehaviour
 
             if (soldi.SpendMoney(weapon.cost))
             {
-                weapon.EquipWeapon();
+                // Riattiva l'arma precedente nel negozio se esiste
+                if (equippedWeapon != null)
+                {
+                    equippedWeapon.gameObject.SetActive(true);
+
+
+                }
+
+                EquipWeapon(weapon);
+                // Disattiva l'arma acquistata
+                weapon.gameObject.SetActive(false);
+                if (lastWeaponSold != null)
+                {
+                    lastWeaponSold.gameObject.SetActive(true);
+                }
+                lastWeaponSold = weapon;
             }
-            else
-            {
-                Debug.Log("Insufficient funds.");
-            }
-        }
-        else
-        {
-            Debug.Log("PlayerCurrency component not found on player.");
+
+
+
         }
     }
-
 
     private void EquipWeapon(Weapon weapon)
     {
-        // Disattiva tutte le altre armi (se presenti)
-        foreach (Transform child in transform)
+        // Disattiva tutte le armi precedenti sul giocatore
+        foreach (Transform child in weaponHoldPoint)
         {
-            Weapon childWeapon = child.GetComponent<Weapon>();
-            if (childWeapon != null)
-            {
-                childWeapon.gameObject.SetActive(false);
-            }
+            Destroy(child.gameObject);
         }
 
-        // Attiva l'arma acquistata
-        weapon.gameObject.SetActive(true);
+        // Instanzia la nuova arma e attaccala al giocatore
+        GameObject newWeaponInstance = Instantiate(weapon.gameObject, weaponHoldPoint);
+        newWeaponInstance.transform.localPosition = Vector3.zero;
+        newWeaponInstance.transform.localRotation = Quaternion.identity;
+
+        // Imposta la nuova arma come l'arma equipaggiata
+        equippedWeapon = newWeaponInstance.GetComponent<Weapon>();
 
         // Logica di equipaggiamento aggiuntiva
         Debug.Log($"Equipped {weapon.weaponName}");
+
+
+        currentWeapon = weapon;
     }
 }
+
+
