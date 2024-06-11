91639c6a9a02c54e8c204e0c92c965428e6ef6f0
AndreaCeretti <cerettiandrea13@gmail.com>
2024-06-11 10:58:25 +0200

    ChangeScene V1.1
    This version remove the string serialized field and use a enum serialized field.
    In this way you need to manually add the scene in the enum and update a switch, but it will be more difficult to do a mistake instead of a string

--- The Heist/Assets/Scripts/Utility/ChangeScene/ChangeScene.cs
+++ The Heist/Assets/Scripts/Utility/ChangeScene/ChangeScene.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+enum Scenes
+{
+    LobbyScene,
+    EasyMapScene
+}
+
 public class ChangeScene : MonoBehaviour
 {
-
     [SerializeField]
-    private string sceneToLoad;
-
-    private Coroutine changeSceneCoroutine;
+    Scenes scene = new Scenes();
     [SerializeField]
     private bool changeSceneStarter;
+    
+    private Coroutine changeSceneCoroutine;
+    private string sceneToLoad;
 
     public bool ChangeSceneStarter{
         get { return changeSceneStarter; }
@@ -20,6 +27,14 @@ public class ChangeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (scene)
+        {
+            case Scenes.LobbyScene: sceneToLoad = "LobbyScene";
+                break;            
+            case Scenes.EasyMapScene: sceneToLoad = "EasyMapScene";
+                break;
+        }
+        
         if (changeSceneCoroutine != null) return;
         if (sceneToLoad == null) return;
         if (changeSceneStarter) changeSceneCoroutine = StartCoroutine(ChangeSceneCoroutine());
